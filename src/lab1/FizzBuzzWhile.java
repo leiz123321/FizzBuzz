5245a3f5c3cf2b7c16860575594a765f8e6f05dd
Lei Zhang <leiz123321@gmail.com>
2026-09-15 19:06:56 -0400

    added a main file

--- src/lab1/FizzBuzzWhile.java
+++ src/lab1/FizzBuzzWhile.java
@@ -11,7 +11,7 @@ class FizzBuzzWhile {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    static void doFizzBuzz(int i) {
 
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
